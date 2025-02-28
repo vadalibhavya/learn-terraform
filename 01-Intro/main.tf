55b61ebf71d929fbbb6627827102dc17834249dc
avadaliDevOps <adityavittal92@gmail.com>
2025-02-28 12:31:04 +0530

    update

--- 01-Intro/main.tf
+++ 01-Intro/main.tf
@@ -24,3 +24,16 @@ output "first_name" {
 output "last_name" {
     value = var.names[2]
 }
+
+#map varaiables
+
+variable "person" {
+    default = {
+        name = "John"
+        age = 30
+        city = "New York"
+    }
+}
+output "name" {
+    value = "Name is ${var.person["name"]}, Age is ${var.person["age"]}, City is ${var.person["city"]}"
+}
